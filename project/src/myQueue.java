442250601c869dbfb6f15d755438f9208dce6e30
yihangZhu1729 <yihang_zhu@outlook.com>
2022-11-01 17:08:11 +0800

    queue

---
+++ project/src/myQueue.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class myQueue {
+    private int size;
+    private float queue[];
+    private int head;
+    private int tail;
+
+    myQueue(int _size){
+        size = _size;
+        queue = new float[size];
+        Arrays.fill(queue, 0);
+        head = 0;
+        tail = size-1;
+    }
+
+    public void add(float element){
+        queue[head] = element;
+        tail = head;
+        head += 1;
+        if(head == size){
+            head = 0;
+        }
+    }
+
+    public float dot_product(float[] preamble){
+        int idx;
+        float sum = 0.0f;
+        for(int i=0; i<size; i++){
+            idx = (i+head) % size;
+            sum += queue[idx] * preamble[i];
+        }
+        return sum;
+    }
+
+}
