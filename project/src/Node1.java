57e0934e506df5bf59d19de7bd0cd1e71f5ac369
yihangZhu1729 <yihang_zhu@outlook.com>
2022-11-26 20:20:07 +0800

    bug fix

--- project/src/Node1.java
+++ project/src/Node1.java
@@ -1,5 +1,10 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Node1 {
-    public static void node1_send(){
+    public static void node1_send() throws IOException {
         AudioHw audioHw = new AudioHw();
         audioHw.init();
         audioHw.start();
@@ -30,7 +35,7 @@ public class Node1 {
         audioHw.stop();
     }
 
-    public static void node1_receive(){
+    public static void node1_receive() throws IOException {
         AudioHw audioHw = new AudioHw();
         audioHw.init();
         audioHw.start();
@@ -48,7 +53,26 @@ public class Node1 {
         byte[] bytes = receiver.writeFile("src/OUTPUT.txt");
         audioHw.stop();
     }
-    public static void main(final String[] args) {
+
+    public static void node1_ICMP_ECHO() throws IOException {
+        AudioHw audioHw = new AudioHw();
+        audioHw.init();
+        audioHw.start();
+
+        DecodeThread decodeThread  = new DecodeThread(audioHw, null, null, Config.NODE_1_CODE);
+        decodeThread.start();
+
+        byte[] payload = Config.DEFAULT_PAYLOAD.getBytes();
+        ArrayList<Integer> data = (ArrayList<Integer>) Arrays.stream(Util.bytesToBits(payload)).boxed().collect(Collectors.toList());
+        float[] track = SW_Sender.frameToTrack(data, Config.NODE_2_CODE, Config.NODE_1_CODE, Config.TYPE_ICMP_ECHO, 0, false,
+                Util.ipToLong(Config.node3_IP), Util.ipToLong(Config.node1_IP), 1, 0, data.size(), (int)System.currentTimeMillis());
+
+        audioHw.PHYSend(track, false);
+        decodeThread.stopDecoding();
+        audioHw.stop();
+    }
+
+    public static void main(final String[] args) throws IOException {
         node1_receive();
     }
 }
